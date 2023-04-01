05702f6c3484f2e7269f9ced380f6d2f6bc75925
Lynda Douglas <lynda.p.douglas@gmail.com>
2023-03-31 23:45:10 -0600

    31-3-23

--- app/src/main/java/com/example/lb_app/MainActivity3.java
+++ app/src/main/java/com/example/lb_app/MainActivity3.java
@@ -1,9 +1,17 @@
 package com.example.lb_app;
 
+import static com.example.lb_app.Structure_BBDD.TABLE2;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -44,6 +52,212 @@ public class MainActivity3 extends AppCompatActivity {
         coment2=(EditText) findViewById(R.id.coment);
 
 
+        btninsert2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    SQLiteDatabase db = helper.getWritableDatabase();
+                    ContentValues values = new ContentValues();
+                    values.put(Structure_BBDD.COLUMNA2, transid.getText().toString());
+                    values.put(Structure_BBDD.COLUMNA3, date2.getText().toString());
+                    values.put(Structure_BBDD.COLUMNA4, descrip2.getText().toString());
+                    values.put(Structure_BBDD.COLUMNA5, amt2.getText().toString());
+                    values.put(Structure_BBDD.COLUMNA6, price2.getText().toString());
+                    values.put(Structure_BBDD.COLUMNA7, total2.getText().toString());
+                    values.put(Structure_BBDD.COLUMNA8, coment2.getText().toString());
+
+                    long newRowId = db.insert(TABLE2, null, values);
+                    Toast.makeText(getApplicationContext(), "The register was saved with ID: " + newRowId, Toast.LENGTH_LONG).show();
+
+                    //Clear text from fields
+                    id.setText("");
+                    transid.setText("");
+                    date2.setText("");
+                    descrip2.setText("");
+                    amt2.setText("");
+                    price2.setText("");
+                    total2.setText("");
+                    coment2.setText("");
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        btnSearch2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = helper.getReadableDatabase();
+                String[] projection = {
+                        Structure_BBDD.COLUMNA2,
+                        Structure_BBDD.COLUMNA3,
+                        Structure_BBDD.COLUMNA4,
+                        Structure_BBDD.COLUMNA5,
+                        Structure_BBDD.COLUMNA6,
+                        Structure_BBDD.COLUMNA7,
+                        Structure_BBDD.COLUMNA8
+                };
+
+                String selection = Structure_BBDD.COLUMNAID + " = ?";
+                String[] selectionArgs = {id.getText().toString()};
+
+
+                try {
+                    Cursor cursor = db.query(
+                            TABLE2,   // The table to query
+                            projection,             // The array of columns to return (pass null to get all)
+                            selection,              // The columns for the WHERE clause
+                            selectionArgs,          // The values for the WHERE clause
+                            null,                   // don't group the rows
+                            null,                   // don't filter by row groups
+                            null               // The sort order
+                    );
+                    cursor.moveToFirst();
+                    transid.setText("");
+                    date2.setText("");
+                    descrip2.setText("");
+                    amt2.setText("");
+                    price2.setText("");
+                    total2.setText("");
+                    coment2.setText("");
+
+
+                    transid.setText(cursor.getString(0));
+                    date2.setText(cursor.getString(1));
+                    descrip2.setText(cursor.getString(2));
+                    amt2.setText(cursor.getString(3));
+                    price2.setText(cursor.getString(4));
+                    total2.setText(cursor.getString(5));
+                    coment2.setText(cursor.getString(6));
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "ERROR: Could not find the requested register. ", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        btnUpdate2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = helper.getReadableDatabase();
+// New value for one column
+                ContentValues values = new ContentValues();
+                values.put(Structure_BBDD.COLUMNAID, id.getText().toString());
+                values.put(Structure_BBDD.COLUMNA2, transid.getText().toString());
+                values.put(Structure_BBDD.COLUMNA3, date2.getText().toString());
+                values.put(Structure_BBDD.COLUMNA4, descrip2.getText().toString());
+                values.put(Structure_BBDD.COLUMNA5, amt2.getText().toString());
+                values.put(Structure_BBDD.COLUMNA6, price2.getText().toString());
+                values.put(Structure_BBDD.COLUMNA7, total2.getText().toString());
+                values.put(Structure_BBDD.COLUMNA8,coment2.getText().toString());
+                String selection = Structure_BBDD.COLUMNAID + " LIKE ?";
+                String[] selectionArgs = {id.getText().toString()};
+
+                int count = db.update(
+                        TABLE2,
+                        values,
+                        selection,
+                        selectionArgs);
+                Toast.makeText(getApplicationContext(), "Register " +id.getText()+ " has been successfully updated.", Toast.LENGTH_LONG).show();
+            }
+        });
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    SQLiteDatabase db = helper.getWritableDatabase();
+                    // Define 'where' part of query.
+                    String selection = Structure_BBDD.COLUMNAID + " LIKE ?";
+// Specify arguments in placeholder order.
+                    String[] selectionArgs = {id.getText().toString()};
+// Issue SQL statement.
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity3.this);
+                    builder.setMessage("Are you sure you would like to delete this register?").setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    db.delete(TABLE2, selection, selectionArgs);
+                                    Toast.makeText(getApplicationContext(), "The register has been deleted", Toast.LENGTH_LONG).show();
+                                    id.setText("");
+                                    transid.setText("");
+                                    date2.setText("");
+                                    descrip2.setText("");
+                                    amt2.setText("");
+                                    price2.setText("");
+                                    total2.setText("");
+                                    coment2.setText("");
+                                }
+                            })
+                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                                public void onCick(DialogInterface dialogInterface, int i) {
+
+                                }
+
+                            }).show();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    id.setText("");
+                    transid.setText("");
+                    date2.setText("");
+                    descrip2.setText("");
+                    amt2.setText("");
+                    price2.setText("");
+                    total2.setText("");
+                    coment2.setText("");
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+        btnSendrecipt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    transid.getText();
+                    date2.getText();
+                    descrip2.getText();
+                    amt2.getText();
+                    price2.getText();
+                    total2.getText();
+
+                    Intent intent=new Intent(Intent.ACTION_SEND);
+                    intent.setType("*/*");
+                    intent.putExtra(Intent.EXTRA_SUBJECT,"Transaction Notification from LynBee´s");
+                    intent.putExtra(Intent.EXTRA_TEXT,"Date: "+date2.getText()+" " +
+                            "\n Receipt number: "+transid.getText()+" " +
+                            "\n Description: "+descrip2.getText()+" " +
+                            "\n Amount: "+amt2.getText()+" " +
+                            "\n Price: "+price2.getText()+
+                            "\n Total: "+total2.getText()+" "+
+                            "\n \n  Thank you for your purchase!");
+                    if(intent.resolveActivity(getPackageManager()) !=null){
+                        startActivity(intent);
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        shistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(MainActivity3.this, MainActivity5.class);
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
     private void MainMenu() {
         try {
