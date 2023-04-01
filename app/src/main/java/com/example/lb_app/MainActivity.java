b2fe6ce43efe6b9454e89031df65410774a2edb8
Lynda Douglas <lynda.p.douglas@gmail.com>
2023-03-31 22:39:17 -0600

    31-3-23

--- app/src/main/java/com/example/lb_app/MainActivity.java
+++ app/src/main/java/com/example/lb_app/MainActivity.java
@@ -1,14 +1,225 @@
 package com.example.lb_app;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnHR,btnS,btnE;
+    ActivityResultLauncher<String[]> mPermissionResultlauncher;
+    private boolean isReadPermissionGranted = false;
+    private boolean isLocationPermissionGranted = false;
+    private boolean isWritePermissionGranted = false;
+    private boolean isReadCalendarPermissionGranted = false;
+    private boolean isWriteCalendarPermissionGranted = false;
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.mmenu:
+                MainMenu();
+                return true;
+
+            case R.id.hivrec:
+                HiveRecords();
+                return true;
+
+            case R.id.ventas:
+                s:
+                Sales();
+                return true;
+
+            case R.id.gastos:
+                s:
+                Expenditure();
+                return true;
+            case R.id.gastoshstry:
+                s:
+                ExpenditureHistory();
+                return true;
+            case R.id.salesresum:
+                s:
+                SalesResume();
+                return true;
+            case R.id.event:
+                s:
+                PlanEvent();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnHR=(Button) findViewById(R.id.hr);
+        btnS=(Button) findViewById(R.id.sls);
+        btnE=(Button)findViewById(R.id.exp);
+
+        btnE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity4.class);
+                startActivity(intent);
+            }
+        });
+        btnHR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        btnS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(MainActivity.this, MainActivity3.class);
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        mPermissionResultlauncher = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), new ActivityResultCallback<Map<String, Boolean>>() {
+            @Override
+            public void onActivityResult(Map<String, Boolean> result) {
+
+                if (result.get(android.Manifest.permission.READ_EXTERNAL_STORAGE) != null) {
+                    isReadPermissionGranted = Boolean.TRUE.equals(result.get(android.Manifest.permission.READ_EXTERNAL_STORAGE));
+                }
+                if (result.get(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != null) {
+                    isReadPermissionGranted = Boolean.TRUE.equals(result.get(android.Manifest.permission.WRITE_EXTERNAL_STORAGE));
+                }
+                if (result.get(android.Manifest.permission.ACCESS_FINE_LOCATION) != null) {
+                    isReadPermissionGranted = Boolean.TRUE.equals(result.get(android.Manifest.permission.ACCESS_FINE_LOCATION));
+                }
+                if (result.get(android.Manifest.permission.ACCESS_COARSE_LOCATION) != null) {
+                    isReadPermissionGranted = Boolean.TRUE.equals(result.get(android.Manifest.permission.ACCESS_COARSE_LOCATION));
+                }
+                if (result.get(android.Manifest.permission.READ_CALENDAR) != null) {
+                    isReadCalendarPermissionGranted = Boolean.TRUE.equals(result.get(android.Manifest.permission.READ_CALENDAR));
+                }
+                if (result.get(android.Manifest.permission.WRITE_CALENDAR) != null) {
+                    isWriteCalendarPermissionGranted = Boolean.TRUE.equals(result.get(Manifest.permission.WRITE_CALENDAR));
+                }
+            }
+        });
+        requestPermission();
+    }
+    private void requestPermission() {
+        isReadPermissionGranted = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+        isWritePermissionGranted = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+        isLocationPermissionGranted = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+        isReadCalendarPermissionGranted = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) == PackageManager.PERMISSION_GRANTED;
+        isWriteCalendarPermissionGranted = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR) == PackageManager.PERMISSION_GRANTED;
+
+        List<String> permissionRequest = new ArrayList<String>();
+        if (!isReadPermissionGranted) {
+            permissionRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+        if (!isLocationPermissionGranted) {
+            permissionRequest.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if (!isWritePermissionGranted) {
+            permissionRequest.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!isReadCalendarPermissionGranted) {
+            permissionRequest.add(Manifest.permission.READ_CALENDAR);
+        }
+        if (!isWriteCalendarPermissionGranted) {
+            permissionRequest.add(Manifest.permission.WRITE_CALENDAR);
+        }
+    }
+    private void MainMenu() {
+        try {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+        }
+    }
+    private void HiveRecords() {
+        try {
+            Intent intent = new Intent(this, MainActivity2.class);
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void Sales() {
+        try {
+            Intent intent = new Intent(this, MainActivity3.class);
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void Expenditure() {
+        try {
+            Intent intent = new Intent(this, MainActivity4.class);
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+        }
+    }
+    private void ExpenditureHistory() {
+        try {
+            Intent intent = new Intent(this, MainActivity8.class);
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+        }
+    }
+    private void SalesResume() {
+        try {
+            Intent intent = new Intent(this, MainActivity5.class);
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+        }
+    }
+    private void PlanEvent() {
+        try {
+            Intent intent=new Intent(this,MainActivity6.class);
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+        }
     }
 }
