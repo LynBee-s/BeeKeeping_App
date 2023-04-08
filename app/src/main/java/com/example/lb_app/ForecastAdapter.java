218065e3ec4122519d05c3a90355fa1a2987947b
Lynda Douglas <lynda.p.douglas@gmail.com>
2023-04-07 21:46:29 -0600

    7-4-23

--- app/src/main/java/com/example/lb_app/ForecastAdapter.java
+++ app/src/main/java/com/example/lb_app/ForecastAdapter.java
@@ -1,5 +1,53 @@
 package com.example.lb_app;
 
-public class ForecastAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ViewHolder> {
+    LayoutInflater inflater;
+    ArrayList<Forecast> data;
+    public  ForecastAdapter(Context context){
+        this.inflater=LayoutInflater.from(context);
+    }
+    public  ForecastAdapter(ArrayList<Forecast> data) {
+        this.data = data;
+    }
+    @NonNull
+    @Override
+    public ForecastAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.forecast,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ForecastAdapter.ViewHolder holder, int position) {
+        holder.date.setText((data.get(position).getDate()));
+        holder.day.setText((data.get(position).getDay()));
+        holder.temp.setText((data.get(position).getTemperature()));
+        holder.cond.setText((data.get(position).getConditions()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView date,day,temp,cond;
+        public ViewHolder(final View itemView) {
+            super(itemView);
+            date=(TextView) itemView.findViewById(R.id.rvid);
+            day=(TextView) itemView.findViewById(R.id.rvhid);
+            temp=(TextView) itemView.findViewById(R.id.rvdate);
+            cond=(TextView)itemView.findViewById(R.id.rvframes);
+
+        }
+    }
 }
--- app/src/main/java/com/example/lb_app/MainActivity9.java
+++ app/src/main/java/com/example/lb_app/MainActivity9.java
@@ -1,14 +1,167 @@
 package com.example.lb_app;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.example.lb_app.Structure_BBDD.TABLE2;
+import static com.example.lb_app.Structure_BBDD.TABLE4;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity9 extends AppCompatActivity {
+    HiveListHelper helper;
+    private RecyclerView recyclerView;
+    public ArrayList<Forecast> data;
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+    case R.id.mmenu:
+        MainMenu();
+        return true;
+
+    case R.id.hivrec:
+        HiveRecords();
+        return true;
+
+    case R.id.ventas:
+        s:
+        Sales();
+        return true;
+
+    case R.id.gastos:
+        s:
+        Expenditure();
+        return true;
+    case R.id.gastoshstry:
+        s:
+        ExpenditureHistory();
+        return true;
+    case R.id.salesresum:
+        s:
+        SalesResume();
+        return true;
+    case R.id.event:
+        s:
+        PlanEvent();
+        return true;
+    case R.id.geoloc:
+        s:
+        HiveMap();
+        return true;
+    default:
+        return super.onOptionsItemSelected(item);
+    }
+}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main9);
+        HiveDB_Helper hiveDB_helper=new HiveDB_Helper(MainActivity9.this);
+        helper=new HiveListHelper(getApplicationContext(),"LBDB.db",null,1);
+        recyclerView=(RecyclerView)findViewById(R.id.recycler_view8);
+        data=new ArrayList<>();
+
+    }
+    private void getAdapter(){
+        ForecastAdapter adapter = new ForecastAdapter(data);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+    }
+    private  void  getInfo(){
+        SQLiteDatabase db=helper.getReadableDatabase();
+       Forecast forecast=null;
+        Cursor cur = db.rawQuery("select * from "+TABLE4 ,null);
+        while (cur.moveToNext()) {
+           forecast=new Forecast();
+            forecast.setDate(cur.getString(0));
+            forecast.setDay(cur.getString(1));
+            forecast.setTemperature(cur.getString(2));
+            forecast.setConditions(cur.getString(3));
+        }
+    }
+    private void MainMenu() {
+        try {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+        }
+    }
+    private void HiveRecords() {
+        try {
+            Intent intent = new Intent(this, MainActivity2.class);
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void Sales() {
+        try {
+            Intent intent = new Intent(this, MainActivity3.class);
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+        }
+    }
+    private void Expenditure() {
+        try {
+            Intent intent = new Intent(this, MainActivity4.class);
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+        }
+    }
+    private void ExpenditureHistory() {
+        try {
+            Intent intent = new Intent(this, MainActivity8.class);
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+        }
+    }
+    private void SalesResume() {
+        try {
+            Intent intent = new Intent(this, MainActivity5.class);
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+        }
+    }
+    private void PlanEvent() {
+        try {
+            Intent intent=new Intent(this,MainActivity6.class);
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+        }
+    }
+    private void HiveMap() {
+        try {
+            Intent intent=new Intent(this,MainActivity7.class);
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+        }
     }
 }
